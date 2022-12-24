bf030979c76c26eff7bc43d92f36f3c3580e942f
Luka <nozadzeluka2002@gmail.com>
2022-12-24 15:22:30 +0400

    finish: checkout modal responsive design

--- src/Pages/UI/CheckoutModal.jsx
+++ src/Pages/UI/CheckoutModal.jsx
@@ -11,18 +11,18 @@ const CheckoutModal = () => {
     setIsClicked(!isClicked);
   };
   return (
-    <div className="font-manrope w-[327px] bg-white pb-[31px] pt-[32px] rounded-lg border-2 px-8 absolute ">
-      <div className="w-16 h-16 rounded-full bg-[#D87D4A] flex justify-center items-center mb-[23px]">
+    <div className="font-manrope w-[327px] bg-white pb-[31px] pt-[32px] rounded-lg border-2 px-8 absolute md:w-[540px] md:pt-[48px] md:pb-[48px] ">
+      <div className="w-16 h-16 rounded-full bg-[#D87D4A] flex justify-center items-center mb-[23px] md:mb-[33px]">
         <img src={correctionMarkPath} alt="" />
       </div>
-      <h2 className="uppercase font-bold text-[24px] leading-[27px] tracking-[0.9px] pb-[16px] ">
+      <h2 className="uppercase font-bold text-[24px] leading-[27px] tracking-[0.9px] pb-[16px] md:text-[32px] md:leading-[36px] md:tracking-[1.14px] md:w-[284px] md:pb-[24px] ">
         thank you for your order
       </h2>
       <span className="font-medium text-[15px] leading-[25px] opacity-50">
         You will receive an email confirmation shortly.
       </span>
-      <div className="pt-[24px] flex flex-col">
-        <div className="bg-[#F1F1F1] pt-[33px] pl-[35px] pr-[24px]  pb-[25px] rounded-md">
+      <div className="pt-[24px] flex flex-col md:flex-row  md:pt-[33px]">
+        <div className="bg-[#F1F1F1] pt-[33px] pl-[35px] pr-[24px]  pb-[25px] rounded-md md:min-h-[140px] md:w-[246px] md:rounded-tr-none md:rounded-br-none">
           <ul>
             <li className="flex justify-between items-center  ">
               <div className="flex gap-4 items-center ">
@@ -86,8 +86,8 @@ const CheckoutModal = () => {
             </div>
           </ul>
         </div>
-        <div className="bg-black text-white rounded-b-md mb-[23px] ">
-          <div className="flex flex-col gap-2 pl-[24px] pt-[15px] pb-[19px]">
+        <div className="bg-black text-white rounded-b-md mb-[23px] md:w-[198px] md:min-h-[140px] md:mb-0 md:flex md:items-center md:rounded-bl-none md:rounded-tr-md  ">
+          <div className="flex flex-col gap-2 pl-[24px] pt-[15px] pb-[19px] md:pt-0 ">
             <span className="uppercase font-medium text-[15px] leading-[25px] opacity-50">
               grand total
             </span>
@@ -100,7 +100,7 @@ const CheckoutModal = () => {
       <Link to="/">
         <button
           onClick={() => dispatch(CheckoutActions.modalOff())}
-          className="uppercase text-[13px] leading-[18px] tracking-[1px] w-[259px] h-[48px] text-white font-bold bg-[#D87D4A] hover:bg-[#FBAF85] "
+          className="uppercase text-[13px] leading-[18px] tracking-[1px] w-[259px] h-[48px] text-white font-bold bg-[#D87D4A] hover:bg-[#FBAF85] md:w-[444px] md:mt-[46px] "
         >
           back to home
         </button>
